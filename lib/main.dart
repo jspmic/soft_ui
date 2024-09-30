7be9f9deeb86b487526b0dc86de48cdc44a1508a
Jaspe Michael <micaeljaspe@gmail.com>
2024-10-01 01:45:22 +0200

    Preparation for the project and the login page implementation

--- lib/main.dart
+++ lib/main.dart
@@ -5,48 +5,79 @@ void main() {
   runApp(const Home());
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String? username = "Nom d'utilisateur...";
+  String? pssw = "Mot de passe...";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Soft",
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
           useMaterial3: true,
         ),
         home: Scaffold(
           backgroundColor: Colors.grey[900],
-            appBar: AppBar(
-              elevation: 0.0,
-              title: const Text("", style: TextStyle(color: Colors.white),),
-              centerTitle: true,
-              backgroundColor: Colors.lightGreen[900],
-            ),
-          body: const Padding(
-            padding: EdgeInsets.all(20.0),
+          body: Container(
+            padding: const EdgeInsets.all(20.0),
+            margin: const EdgeInsets.all(20.0),
+            color: Colors.grey[900],
             child: Column(
               children: [
-                Text(
+                const Text(
                       "Bienvenue",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 30
                       ),
                     ),
-                    SizedBox(height: 40,),
+                    const SizedBox(height: 40,),
                     TextField(
                       decoration: InputDecoration(
-                        hintText: "Nom d'utilisateur..."
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0))
+                        ),
+                        hintText: "$username",
                       ),
+                      cursorColor: Colors.amber,
+                      cursorHeight: 15.0,
+                      cursorWidth: 1.0,
                     ),
-                    SizedBox(height: 30.0),
+                    const SizedBox(height: 30.0),
                     TextField(
                       decoration: InputDecoration(
-                          hintText: "Mot de passe..."
+                            border: const OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(20.0))
+                            ),
+                          hintText: "$pssw"
                       ),
+                      cursorColor: Colors.amber,
+                      cursorHeight: 15.0,
+                      cursorWidth: 1.0,
                     ),
+                const SizedBox(height: 30.0,),
+                TextButton(
+                    onPressed: () { },
+                    style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.lightGreen),
+                      elevation: WidgetStatePropertyAll(50.0)
+                    ),
+                    child: const Text(
+                        "Se connecter",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      ),
+                    ))
                   ],
             ),
           ),
