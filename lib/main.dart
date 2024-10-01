523f47a4e92de259e8a1d88a88884dc7775f766a
jspmic <jaspe@jspmic.jspmic>
2024-10-01 22:26:39 +0200

    Preparation for the implementation of the excel worksheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:Soft/excel_fields.dart';
 
-void main() {
-  runApp(const Home());
-}
+void main() => runApp(const Home());
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,14 +12,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   String? username = "Nom d'utilisateur...";
   String? pssw = "Mot de passe...";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Soft",
+        title: "Soft",
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
           useMaterial3: true,
@@ -34,51 +32,47 @@ class _HomeState extends State<Home> {
             child: Column(
               children: [
                 const Text(
-                      "Bienvenue",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30
-                      ),
-                    ),
-                    const SizedBox(height: 40,),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0))
-                        ),
-                        hintText: "$username",
-                      ),
-                      cursorColor: Colors.amber,
-                      cursorHeight: 15.0,
-                      cursorWidth: 1.0,
-                    ),
-                    const SizedBox(height: 30.0),
-                    TextField(
-                      decoration: InputDecoration(
-                            border: const OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(20.0))
-                            ),
-                          hintText: "$pssw"
-                      ),
-                      cursorColor: Colors.amber,
-                      cursorHeight: 15.0,
-                      cursorWidth: 1.0,
-                    ),
-                const SizedBox(height: 30.0,),
+                  "Bienvenue",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    hintText: "$username",
+                  ),
+                  cursorColor: Colors.amber,
+                  cursorHeight: 15.0,
+                  cursorWidth: 1.0,
+                ),
+                const SizedBox(height: 30.0),
+                TextField(
+                  decoration: InputDecoration(
+                      border: const OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(20.0))),
+                      hintText: "$pssw"),
+                  cursorColor: Colors.amber,
+                  cursorHeight: 15.0,
+                  cursorWidth: 1.0,
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
                 TextButton(
-                    onPressed: () { },
+                    onPressed: () {},
                     style: const ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Colors.lightGreen),
-                      elevation: WidgetStatePropertyAll(50.0)
-                    ),
+                        backgroundColor:
+                            WidgetStatePropertyAll(Colors.lightGreen),
+                        elevation: WidgetStatePropertyAll(50.0)),
                     child: const Text(
-                        "Se connecter",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15
-                      ),
+                      "Se connecter",
+                      style: TextStyle(color: Colors.black, fontSize: 15),
                     ))
-                  ],
+              ],
             ),
           ),
         ));
