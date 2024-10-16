51baac8efc48808dced44df0f54b86afec206e04
jspmic <micaeljaspe@gmail.com>
2024-10-16 22:24:47 +0200

    New retrieval method in custom_widgets
    - Added new method `readColline` to read the corresponding `colline` of
    the `district`
    - `ScreenTransition` now requires the fieldColor
    - Renamed the state class `_TransfertScreenState` =>
    `_LivraisonScreenState`
    - Full implementation of the Boucle widget to include the `colline`

--- lib/custom_widgets.dart
+++ lib/custom_widgets.dart
@@ -4,12 +4,35 @@ import 'package:soft/screen2.dart';
 import 'package:soft/rest.dart';
 import 'package:soft/transfert.dart';
 
+Map<int, Iterable<String?>> cache = {};
+Map<String?, Iterable<String?>> cache2 = {};
+
 DateTime? dateSelected;
+bool collineDisponible = false;
 
 // Function to list all the contents of the specified column in the sheet
-Future<Iterable<String?>> list(int column) async{
+void list(int column, {String? district}) async{
   Worksheet workSheet = await Worksheet.fromAsset("assets/worksheet.xlsx");
-  return workSheet.readColumn("Feuille 1", column);
+  if (district == null) {
+    cache[column] = workSheet.readColumn("Feuille 1", column);
+  }
+  else{
+    if (cache2.containsKey(district) == true){
+      return;
+    }
+    cache2[district] = workSheet.readColline("Feuille 1", district);
+  }
+}
+
+void initialize({String? district}){
+  if (district != null){
+    list(DISTRICT+5, district: district);
+    return;
+  }
+  list(STOCK_CENTRAL);
+  list(TYPE_TRANSPORT);
+  list(INPUT);
+  list(DISTRICT);
 }
 
 // Custom DatePicker widget
@@ -45,7 +68,7 @@ class _DatePickerState extends State<DatePicker> {
             ),
           ),
           const SizedBox(height: 10,),
-          Text("Date: ${_date ?? "Aucune date"}",
+          Text("Date: ${_date == null ? "Aucune date" : "${_date?.day}/${_date?.month}/${_date?.year}"}",
               style: TextStyle(fontSize: 18, color: background == Colors.white ? Colors.black
               : Colors.white))
         ],
@@ -58,10 +81,11 @@ class _DatePickerState extends State<DatePicker> {
 class Stock extends StatefulWidget {
   final String hintText;
   final int column;
+  final String? district;
   final Color? background;
   final Function(String) onSelect;
-  const Stock({super.key, required this.hintText, required this.column, required this.background,
-  required this.onSelect});
+  const Stock({super.key, required this.hintText, required this.column,
+    required this.background, this.district, required this.onSelect});
 
   @override
   State<Stock> createState() => _StockState();
@@ -74,11 +98,31 @@ class _StockState extends State<Stock> {
   void initState(){
     _hintCopy = widget.hintText;
     super.initState();
-  }
+    if (widget.district != null){
+      list(DISTRICT+5, district: widget.district);
+    }
+    else {
+      list(widget.column);
+    }
+    }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Iterable<String?>>(future: list(widget.column), builder: (context, snapshot){
+    return SingleChildScrollView(scrollDirection: Axis.horizontal,
+      child: DropdownButton<String?>(items: (widget.district != null ? cache2[widget.district] : cache[widget.column])
+          ?.map((choice){
+        return DropdownMenuItem(value: choice, child: Text(choice.toString()));
+      }).toList(), onChanged: (value){
+        setState(() {
+          _hintCopy = value!;
+          widget.onSelect(value);
+        });
+      }, hint: Text(_hintCopy, style: TextStyle(color: widget.background == Colors.white ? Colors.black
+          : Colors.white)),
+    style: TextStyle(color: widget.background == Colors.white ?  Colors.black
+        : Colors.white)
+    ));
+    /*return FutureBuilder<Iterable<String?>>(future: list(widget.column), builder: (context, snapshot){
       if (snapshot.connectionState == ConnectionState.waiting){
         return CircularProgressIndicator();
       }
@@ -102,7 +146,7 @@ class _StockState extends State<Stock> {
             style: TextStyle(color: widget.background == Colors.white ?  Colors.black
                 : Colors.white)));
       }
-    });
+    });*/
   }
 }
 
@@ -115,15 +159,14 @@ class stockCentralSuivant extends StatefulWidget {
 
 class _stockCentralSuivantState extends State<stockCentralSuivant> {
   List<Widget> _stockWidgets = [];
+  int index = 0;
 
   void newStock(){
     _stockWidgets.add(Stock(hintText: "Stock Central Suivant",
       column: STOCK_CENTRAL,
       background: background,
       onSelect: (value){
-      if (!objTransfert.stock_central_suivants.keys.contains(value)) {
-        objTransfert.stock_central_suivants[value] = value;
-      }
+      objTransfert.stock_central_suivants.add(value);
       },
     ));
   }
@@ -155,7 +198,8 @@ class _stockCentralSuivantState extends State<stockCentralSuivant> {
 }
 
 class Boucle extends StatefulWidget {
-  const Boucle({super.key});
+  final String district;
+  const Boucle({super.key, required this.district});
 
   @override
   State<Boucle> createState() => _BoucleState();
@@ -165,18 +209,25 @@ class _BoucleState extends State<Boucle> {
   List<Widget> _boucle = [];
 
   void create_boucle(){
-    _boucle.add(Stock(hintText: "Input",
-        column: INPUT,
-        background: background,
-      onSelect: (value){}
-    )
-    );
-    _boucle.add(TextField(
-      style: TextStyle(fontSize: 15),
-      decoration: InputDecoration(
-        hintText: "Quantité...",
-      ),
-    ));
+      _boucle.add(Stock(hintText: "Colline",
+          column: DISTRICT+5,
+          district: widget.district,
+          background: background,
+          onSelect: (value) {}
+      )
+      );
+      _boucle.add(Stock(hintText: "Input",
+          column: INPUT,
+          background: background,
+          onSelect: (value) {}
+      )
+      );
+      _boucle.add(TextField(
+        style: TextStyle(fontSize: 15),
+        decoration: InputDecoration(
+          hintText: "Quantité...",
+        ),
+      ));
   }
   @override
   void initState(){
