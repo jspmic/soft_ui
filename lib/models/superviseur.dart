bf7219f9dc101235c99fb60dc4ff176e5e4a07a8
jspmic <micaeljaspe@gmail.com>
2025-07-30 12:42:15 +0200

    feat: added superviseur model

---
+++ lib/models/superviseur.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+Superviseur superviseurFromJson(String str) => Superviseur.fromJson(json.decode(str));
+
+String superviseurToJson(Superviseur data) => json.encode(data.toJson());
+
+class Superviseur {
+	int id;
+	String nom;
+	String nom_utilisateur;
+	String lot;
+	Superviseur({
+	required this.nom_utilisateur,
+	required this.id,
+	required this.lot,
+	required this.nom
+	});
+    factory Superviseur.fromJson(Map<String, dynamic> json) => Superviseur(
+        id: json["id"],
+        lot: json["lot"],
+        nom: json["nom"],
+        nom_utilisateur: json["_n_9032"]
+    );
+
+    Map<String, dynamic> toJson() => {
+        "_n_9032": nom_utilisateur,
+        "lot": lot,
+        "nom": nom,
+    };
+}
